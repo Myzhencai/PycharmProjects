dd3b191813474acbe277827ac6717180a25f6ecf
Myzhencai <Myzhencai@hotmail.com>
2023-04-27 16:25:41 +0800

    init repo

--- tf2rnnlstm/loadckpttf2.py
+++ tf2rnnlstm/loadckpttf2.py
@@ -1,6 +1,7 @@
-
+# H3 芯片測試加載tensorflow模型和
 import tensorflow.compat.v1 as tf
 import numpy as np
+import time 
 
 tf.disable_eager_execution()
 
@@ -16,7 +17,10 @@ input_x = sess.graph.get_tensor_by_name('x:0')
 output = sess.graph.get_tensor_by_name('op_to_store:0')
 # #加載實時數據
 while True:
+    starttime = time.time()
     newx = np.array([124,546,234,243,564,134,535,643,674]).reshape((-1,9))
     test_output = sess.run(output, {input_x: newx})
     pred_y = np.argmax(test_output, 1)
+    endtime = time.time()
+    print("used time :",endtime-starttime)
     print("pred_y: ",pred_y)
