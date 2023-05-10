be9f1ed264b2a49177ba9cfc9304d9f387439c14
Myzhencai <Myzhencai@hotmail.com>
2023-05-10 18:50:54 +0800

    add socket

--- tf2rnnlstm/socket/H3.py
+++ tf2rnnlstm/socket/H3.py
@@ -5,15 +5,27 @@ import threading
 import numpy as np
 # 此處代碼放在H3上面
 
-def leftserial():
+flag = True
+# 生成socket对象
+client = socket.socket()
+clientcontrol = socket.socket()
+# ipaddress = input("請輸入電腦網絡的ip地址：")
+ipaddress = '172.16.2.55'
+# 链接要链接的ip和port（端口）
+client.connect((ipaddress, 6868))
+print("connect1")
+openleft = True
+openright = True
+
+def leftserial(client):
     serleft = serial.Serial("/dev/ttyS2", 115200, timeout=0.01)
-    serwriteleft = serial.Serial("/dev/ttyS0", 115200, timeout=0.01)
+    # serwriteleft = serial.Serial("/dev/ttyS0", 115200, timeout=0.01)
     serleft.flushInput()
-    serwriteleft.flushInput()
+    # serwriteleft.flushInput()
     while True:
         currentdataleft = serleft.readline()
         if currentdataleft != b'':
-            serwriteleft.write(currentdataleft)
+            client.send(currentdataleft)
 
 def rightserial():
     serright = serial.Serial("/dev/ttyS2", 115200, timeout=0.01)
@@ -25,36 +37,71 @@ def rightserial():
         if currentdataright != b'':
             serwriteleft.write(currentdataright)
 
-flag = True
-# 生成socket对象
-client = socket.socket()
-ipaddress = input("請輸入電腦網絡的ip地址：")
-# 链接要链接的ip和port（端口）
-client.connect((ipaddress, 6666))
+# def controlflow():
+#     global openleft
+#     global openright
+#     clientcontrol = socket.socket()
+#     clientcontrol.connect(("172.16.2.55", 9987))
+#     print("connect2 ")
+#     while True:
+#         flowComand = clientcontrol.recv(1024).decode()
+#         print(flowComand)
+#         if flowComand == "closeleft":
+#             openleft = False
+#         if flowComand == "closeright":
+#             openright = False
+#
+# flowcontrol = threading.Thread(target=controlflow,name='LoopThread')
+# flowcontrol.start()
 # while循环
 while flag:
     commandfromPC = client.recv(1024).decode()
     if commandfromPC == 'openleft':
-        print("opened left serial ")
+        print("打開左邊串口")
+        serleft = serial.Serial("/dev/ttyS2", 115200, timeout=0.01)
+        serleft.flushInput()
         client.send('openedleft'.encode())
+        while openleft:
+            currentdataleft = serleft.readline()
+            if currentdataleft != b'':
+                # print("left :",currentdataleft)
+                client.send(currentdataleft)
+                leftstatue = client.recv(1024).decode()
+                if leftstatue ==  'leftdata':
+                    print("在抓取左邊Sensor數據 ")
+                if leftstatue ==  'finishleft':
+                    print("完成左邊數據的抓取")
+                    openleft = False
+        print("關掉左邊的串口")
+        serleft.close()
 
     elif commandfromPC == 'openright':
-        print("open right serial ")
+        print("打開右邊串口")
+        serright = serial.Serial("/dev/ttyS1", 115200, timeout=0.01)
+        serright.flushInput()
         client.send('openedright'.encode())
+        while openright:
+            currentdataright = serright.readline()
+            if currentdataright != b'':
+                # print("left :",currentdataleft)
+                client.send(currentdataright)
+                rightstatue = client.recv(1024).decode()
+                if rightstatue == 'rightdata':
+                    print("在抓取右邊Sensor數據 ")
+                if rightstatue == 'finishright':
+                    print("完成右邊數據的抓取")
+                    openright = False
+        print("關掉右邊的串口")
+        serright.close()
 
-    elif commandfromPC == 'closeleft':
-        print("close left serial ")
-        client.send('closedleft'.encode())
 
-    elif commandfromPC == 'closeright':
-        print("close right serial ")
-        client.send('closedright'.encode())
     elif commandfromPC == 'closeall':
         print("close socket")
         flag = False
+        client.shutdown(2)
+        client.close()
+        print('Server Closed')
     else:
         print("等待PC端指令")
 
 # 关闭socket链接
-client.close()
-print('Server Closed')
--- tf2rnnlstm/socket/PC.py
+++ tf2rnnlstm/socket/PC.py
@@ -1,83 +1,250 @@
 import socket
-import tqdm
+import numpy as np
+
+datasaverleft = []
+datasaverright = []
+def getleftdata(areaid,savetest,Savepath,dataenoughnum):
+    datasaverleft.clear()
+    currentnum = 0
+    while currentnum < dataenoughnum:
+        # socket 讀取一行
+        currentdataleft = conn.makefile().readline()
+        # 返回信息已經接到了數據
+        conn.send('leftdata'.encode())
+        # 處理數據
+        currentdatasaverleftleftold = currentdataleft.split('\n')[0]
+        currentdatasaverleftleft = currentdatasaverleftleftold.split(",")
+        inversecurrentdatasaverleftleft =currentdatasaverleftleftold.split(",")
+        inversecurrentdatasaverleftleft.reverse()
+        currentdatasaverleftleft = currentdatasaverleftleft + inversecurrentdatasaverleftleft
+        dataarrayleft = np.array(currentdatasaverleftleft, dtype='float32').reshape((-1, 18))
+        if areaid ==0:
+            print("左邊額頭數據", currentnum)
+            areaarray = np.array([[1,0,0,0]])
+            currentdataAndarea = np.c_[dataarrayleft,areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft,dtype='float32').reshape((-1,22))
+            # print(dataarray)
+            if savetest:
+                np.savetxt(Savepath+"area{0}test.txt".format(areaid),dataarray)
+            else:
+                np.savetxt(Savepath+"area{0}.txt".format(areaid), dataarray)
+            currentnum +=1
+        elif areaid ==1:
+            print("對應左下頜", currentnum)
+            areaarray = np.array([[0, 1, 0, 0]])
+            currentdataAndarea = np.c_[dataarrayleft, areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft, dtype='float32').reshape((-1, 22))
+            if savetest:
+                np.savetxt(Savepath + "area{0}test.txt".format(areaid), dataarray)
+            else:
+                np.savetxt(Savepath + "area{0}.txt".format(areaid), dataarray)
+            currentnum += 1
+        elif areaid ==2:
+            print("對應左邊面部", currentnum)
+            areaarray = np.array([[0, 0, 1, 0]])
+            currentdataAndarea = np.c_[dataarrayleft, areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft, dtype='float32').reshape((-1, 22))
+            if savetest:
+                np.savetxt(Savepath + "area{0}test.txt".format(areaid), dataarray)
+            else:
+                np.savetxt(Savepath + "area{0}.txt".format(areaid), dataarray)
+            currentnum += 1
+        elif areaid ==3:
+            print("左邊眼周", currentnum)
+            areaarray = np.array([[0, 0, 0, 1]])
+            currentdataAndarea = np.c_[dataarrayleft, areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft, dtype='float32').reshape((-1, 22))
+            if savetest:
+                np.savetxt(Savepath + "area{0}test.txt".format(areaid), dataarray)
+            else:
+                np.savetxt(Savepath + "area{0}.txt".format(areaid), dataarray)
+            currentnum += 1
+
+    return areaid
+
+
+def getrightdata(areaid,savetest,Savepath,dataenoughnum):
+    datasaverleft.clear()
+    currentnum = 0
+    while currentnum < dataenoughnum:
+        # socket 讀取一行
+        currentdataleft = conn.makefile().readline()
+        # 返回信息已經接到了數據
+        conn.send('rightdata'.encode())
+        # 處理數據
+        currentdatasaverleftleftold = currentdataleft.split('\n')[0]
+        currentdatasaverleftleft = currentdatasaverleftleftold.split(",")
+        inversecurrentdatasaverleftleft =currentdatasaverleftleftold.split(",")
+        inversecurrentdatasaverleftleft.reverse()
+        currentdatasaverleftleft = currentdatasaverleftleft + inversecurrentdatasaverleftleft
+        dataarrayleft = np.array(currentdatasaverleftleft, dtype='float32').reshape((-1, 18))
+        if areaid ==0:
+            print("右邊額頭數據", currentnum)
+            areaarray = np.array([[1,0,0,0]])
+            currentdataAndarea = np.c_[dataarrayleft,areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft,dtype='float32').reshape((-1,22))
+            # print(dataarray)
+            if savetest:
+                np.savetxt(Savepath+"area{0}test.txt".format(areaid),dataarray)
+            else:
+                np.savetxt(Savepath+"area{0}.txt".format(areaid), dataarray)
+            currentnum +=1
+        elif areaid ==1:
+            print("對應右下頜", currentnum)
+            areaarray = np.array([[0, 1, 0, 0]])
+            currentdataAndarea = np.c_[dataarrayleft, areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft, dtype='float32').reshape((-1, 22))
+            if savetest:
+                np.savetxt(Savepath + "area{0}test.txt".format(areaid), dataarray)
+            else:
+                np.savetxt(Savepath + "area{0}.txt".format(areaid), dataarray)
+            currentnum += 1
+        elif areaid ==2:
+            print("對應右邊面部", currentnum)
+            areaarray = np.array([[0, 0, 1, 0]])
+            currentdataAndarea = np.c_[dataarrayleft, areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft, dtype='float32').reshape((-1, 22))
+            if savetest:
+                np.savetxt(Savepath + "area{0}test.txt".format(areaid), dataarray)
+            else:
+                np.savetxt(Savepath + "area{0}.txt".format(areaid), dataarray)
+            currentnum += 1
+        elif areaid ==3:
+            print("右邊眼周", currentnum)
+            areaarray = np.array([[0, 0, 0, 1]])
+            currentdataAndarea = np.c_[dataarrayleft, areaarray]
+            datasaverleft.append(currentdataAndarea[0])
+            dataarray = np.array(datasaverleft, dtype='float32').reshape((-1, 22))
+            if savetest:
+                np.savetxt(Savepath + "area{0}test.txt".format(areaid), dataarray)
+            else:
+                np.savetxt(Savepath + "area{0}.txt".format(areaid), dataarray)
+            currentnum += 1
+
+    return areaid
+
+
+
 
 flag = True
+
 # 生成socket对象
 server = socket.socket()
+
 # 绑定ip和端口
-# server.close()
-server.bind(('172.16.2.55', 6666))
-# server.close()
+server.bind(('172.16.2.55', 6868))
+
 # 监听绑定的端口
 server.listen()
+
 # 方便识别打印一个我在等待
 print("等待Matrix鏈接...")
-# 这里用两个值接受，因为链接上之后使用的是客户端发来请求的这个实例
-# 所以下面的传输要使用conn实例操作
+
 conn, addr = server.accept()
 
-# 打印链接成功
 print('成功鏈接Matrix')
+finishleft = False
+finishright = False
+enoughNum = 30
+arealist =[0,1,2,3]
 
 # 进入循环
 while True:
     # 打開左邊
     openleft = input("請問打開左邊的串口進行數據收集麼？（yes or no）:").strip()
     if openleft =='yes':
-        # 發送信號打開左邊串口並監聽是否打開
+        # 發送信號打開左邊串口並監聽H3是否打開左邊的串口
         conn.send('openleft'.encode())
         while True:
             openedleft = conn.recv(1024).decode()
             if openedleft == 'openedleft':
-                print("left serial opened ")
-                # 發送關閉左邊串口的信號
-                print("收集左邊的數據")
-                for i in range(100):
-                    print("left",i)
-                conn.send('closeleft'.encode())
-
-                while True:
-                    closedleft = conn.recv(1024).decode()
-                    if closedleft == 'closedleft':
-                        print("left serial closed ")
-                        break
+                print("左邊的串口已經打開並在傳輸數據 ")
+                for areaid in arealist:
+                    if areaid == 0:
+                        while input("請將美容儀器開機放置在額頭區域（左），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("左邊額頭數據")
+                        finishedid = getleftdata(areaid,False, "./data", enoughNum)
+                        # print(finishedid)
+                    if areaid == 1 and finishedid == 0:
+                        while input("請將美容儀器開機放置在下頜線區域（左），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("對應左下頜")
+                        finishedid = getleftdata(areaid,False, "./data", enoughNum)
+                        # print(finishedid)
+                    if areaid == 2 and finishedid == 1:
+                        while input("請將美容儀器開機放置在臉部區域（左），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("對應左邊面部")
+                        finishedid = getleftdata(areaid,False, "./data", enoughNum)
+                    if areaid == 3 and finishedid == 2:
+                        while input("請將美容儀器開機放置在眼周區域（左），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("左邊眼周")
+                        finishedid = getleftdata(areaid,False, "./data", enoughNum)
+                    if finishedid == 3:
+                        print("完成左邊臉部的數據採集")
+                conn.send('finishleft'.encode())
+                finishleft = True
+                # 退出循環
                 break
     elif openleft =='no':
-        openright = input("請問打開右邊的串口進行數據收集麼？（yes or no）:").strip()
-        if openright == 'yes':
-            # 發送信號打開左邊串口並監聽是否打開
-            conn.send('openright'.encode())
-            while True:
-                openedlright = conn.recv(1024).decode()
-                if openedlright == 'openedright':
-                    print("right serial opened ")
-                    print("收集右邊的數據")
-                    # 關閉右邊的串口
-                    for i in range(100):
-                        print("right",i)
-                    conn.send('closeright'.encode())
+        print("不進行左邊sensor的採集")
+    else:
+        print("錯誤的輸入，請稍後重新輸入")
 
-                    while True:
-                        closedright = conn.recv(1024).decode()
-                        if closedright == 'closedright':
-                            print("right serial closed ")
-                            break
-                    break
-        elif openright == 'no':
-            print("do not open left and right")
-            conn.send('closeall'.encode())
-            server.close()
+    openright = input("請問打開右邊的串口進行數據收集麼？（yes or no）:").strip()
+    if openright == 'yes':
+        # 發送信號打開左邊串口並監聽是否打開
+        conn.send('openright'.encode())
+        while True:
+            openedlright = conn.recv(1024).decode()
+            if openedlright == 'openedright':
+                print("右邊的串口已經打開並在傳輸數據 ")
+                for areaid in arealist:
+                    if areaid == 0:
+                        while input("請將美容儀器開機放置在額頭區域（右），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("右邊額頭數據")
+                        finishedid = getrightdata(areaid,False, "./data", enoughNum)
+                        # print(finishedid)
+                    if areaid == 1 and finishedid == 0:
+                        while input("請將美容儀器開機放置在下頜線區域（右），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("對應右下頜")
+                        finishedid = getrightdata(areaid,False, "./data", enoughNum)
+                        # print(finishedid)
+                    if areaid == 2 and finishedid == 1:
+                        while input("請將美容儀器開機放置在臉部區域（右），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("對應右邊面部")
+                        finishedid = getrightdata(areaid,False, "./data", enoughNum)
+                    if areaid == 3 and finishedid == 2:
+                        while input("請將美容儀器開機放置在眼周區域（右），完成後請按Enter鍵 :") != '':
+                            print("重新輸入")
+                        print("右邊眼周")
+                        finishedid = getrightdata(areaid,False, "./data", enoughNum)
+                    if finishedid == 3:
+                        print("完成右邊臉部的數據採集")
+                conn.send('finishright'.encode())
+                finishleft = True
+                # 退出循環
+                break
+    elif openright == 'no':
+        print("不進行右邊sensor的採集")
+    else:
+        print("錯誤的輸入，請稍後重新輸入")
 
-    # data = conn.recv(1024).decode()
+    if openright == 'no' and openleft =='no':
+        conn.send('closeall'.encode())
+        server.shutdown(2)
+        server.close()
+        break
 
-    # 判断
-    # if data != 'q':
-    #     # 打印收到数据
-    #     print('收到：', data)
-    #     # 发送我收到数据了
-    #     conn.send('Send Successed!'.encode())
-    # else:
-    #     # 条件为False
-    #     flag = False
-
-# 关闭socket链接
